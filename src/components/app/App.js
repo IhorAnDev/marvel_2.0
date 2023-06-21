daa856ccdec65354dc8b261d58c45cb2b3fbceb0
IhorAnDev <ihorandev@gmail.com>
2023-06-21 08:54:25 +0300

    lazy loading

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,12 @@
 import AppHeader from "../pages/AppHeader";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {ComicsPage, MainPage, Page404, SingleComicPage} from "../pages";
+import Spinner from "../spiner/Spinner";
+import {lazy, Suspense} from "react";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
 
@@ -9,12 +15,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics/*" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics/*" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,7 +1,8 @@
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 import {Route, Routes} from "react-router-dom";
-import {SingleComicPage} from "./index";
+import SingleComicPage from "./SingleComicPage";
+
 
 const ComicsPage = () => {
     return (
